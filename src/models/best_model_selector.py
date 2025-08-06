c012bf9e4870ecb89453a16c26797bd9b63993a0
Anup Jindal <anup.jindal@sap.com>
2025-08-06 21:32:16 +0530

    best_model

--- src/models/best_model_selector.py
+++ src/models/best_model_selector.py
@@ -13,8 +13,7 @@ settings = Settings()
 model_registry = ModelRegistry()
 
 
-def find_best_regression_model(model_name, no_recent_versions_to_consider: int = 20) -> Dict[str, Any]: 
-    
+def find_top_regression_model_algorithm(model_name, no_recent_versions_to_consider: int = 20) -> Dict[str, Any]:     
 
     models_data = model_registry.get_model_info_by_model_metrics(model_name, no_recent_versions_to_consider)
 
@@ -29,17 +28,15 @@ def find_best_regression_model(model_name, no_recent_versions_to_consider: int =
     for model_info in models_data:
         metrics = model_info.get('metrics')
         if not metrics:
-            print(f"Skipping model version {model_info.get('model_version', 'N/A')} due to missing metrics.")
+            logger.info(f"Skipping model version {model_info.get('model_version', 'N/A')} due to missing metrics.")
             continue
 
         current_r2 = metrics.get('val_r2')
         current_rmse = metrics.get('val_rmse')
 
-        # Ensure both metrics are present and are numbers
+        # Ensure both metrics are present
         if current_r2 is None or current_rmse is None: 
-        #or not isinstance(current_r2, (int, float)) or \
-        #not isinstance(current_rmse, (int, float)):
-            print(f"Skipping model version {model_info.get('model_version', 'N/A')} due to invalid or missing val_r2/val_rmse.")
+            logger.error(f"Skipping model version {model_info.get('model_version', 'N/A')} due to invalid or missing val_r2/val_rmse.")
             continue
 
         # Prioritize higher R2, then lower RMSE for ties
@@ -53,17 +50,15 @@ def find_best_regression_model(model_name, no_recent_versions_to_consider: int =
                 best_model = model_info
 
         if best_model:
-            print(f"\nBest model found:")
-            print(f"  Model Name: {best_model.get('model_name', 'N/A')}")
-            print(f"  Version: {best_model.get('model_version', 'N/A')}")
-            print(f"  R2: {best_model['metrics'].get('val_r2', 'N/A')}")
-            print(f"  RMSE: {best_model['metrics'].get('val_rmse', 'N/A')}")
+            logger.info(f"\nBest model found:")
+            logger.info(f"  Model Name: {best_model.get('model_name', 'N/A')}, Version: {best_model.get('model_version', 'N/A')}")
+            logger.info(f"  R2: {best_model['metrics'].get('val_r2', 'N/A')}, RMSE: {best_model['metrics'].get('val_rmse', 'N/A')}")
         else:
-            print("No best model could be determined from the provided data.")
+            logger.info("No best model could be determined from the provided data.")
 
     return best_model 
  
-def register_best_model_info(lr_best_model: Dict[str, Any], dt_best_model: Dict[str, Any]):
+def fetch_best_model_info(lr_best_model: Dict[str, Any], dt_best_model: Dict[str, Any]):
     
     if dt_best_model is None and lr_best_model is not None:
         return lr_best_model
@@ -91,9 +86,13 @@ def register_best_model_info(lr_best_model: Dict[str, Any], dt_best_model: Dict[
             else:
                 return lr_best_model
 
+def register_best_model(best_model_data: Dict[str, Any],):
+    logger.info("Best model to be registered in registry: {best_model}")
 
 if __name__ == "__main__":
     MODEL_NAME = 'housing_price_predictor'
-    best_model = find_best_regression_model(MODEL_NAME, no_recent_versions_to_consider=20)
-    register_best_model_info(best_model, None)
-    print(best_model)
+    best_lr_model_data = find_top_regression_model_algorithm(MODEL_NAME, no_recent_versions_to_consider=20)
+    best_dt_model_data = None
+
+    best_model_data = fetch_best_model_info(best_lr_model_data, best_dt_model_data)
+    register_best_model(best_model_data)
